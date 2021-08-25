94cb660a8ff5bfa975ce193fc75728e2dc4d770e
ralvescosta <rafael.rac.mg@gmail.com>
2021-08-25 07:58:06 -0300

    feat: controller and app

--- src/applications/something.rs
+++ src/applications/something.rs
@@ -4,18 +4,6 @@ use crate::{
     models::something::SomethingModel, repositories::something::SomethingRepository, view_models,
 };
 
-pub async fn perform(dto: SomethingViewModel) -> SomethingModel {
-    crate::repositories::something::get_somethings();
-
-    return SomethingModel {
-        id: 1,
-        alias: dto.alias,
-        href: dto.href,
-        name: dto.name,
-        number: dto.number,
-    };
-}
-
 #[derive(Debug, Clone)]
 pub struct SomethingUseCase {
     repo: Box<SomethingRepository>,
@@ -25,14 +13,14 @@ impl SomethingUseCase {
     pub fn new(repo: Box<SomethingRepository>) -> SomethingUseCase {
         SomethingUseCase { repo }
     }
-    pub async fn perform(&self) -> SomethingModel {
+    pub async fn perform(&self, view_model: SomethingViewModel) -> SomethingModel {
         self.repo.something();
         SomethingModel {
             id: 1,
-            alias: String::from("Alias"),
-            href: String::from("HRef"),
-            name: String::from("Name"),
-            number: 10,
+            alias: view_model.alias,
+            href: view_model.href,
+            name: view_model.name,
+            number: view_model.number,
         }
     }
 }
--- src/controllers/something.rs
+++ src/controllers/something.rs
@@ -1,22 +1,16 @@
-use actix_web::{get, HttpResponse, Responder};
+use std::sync::Arc;
+
+use actix_web::{post, web, HttpResponse, Responder};
 
 use crate::{
     applications::something::SomethingUseCase, view_models::something::SomethingViewModel,
 };
 
-#[get("/hi")]
-pub async fn index() -> impl Responder {
-    let result = crate::applications::something::perform(SomethingViewModel {
-        alias: String::from("Alias"),
-        href: String::from("Ref"),
-        name: String::from("Name"),
-        number: 16,
-    })
-    .await;
-    HttpResponse::Ok().json(result)
-}
-
-pub async fn something(usecase: &SomethingUseCase) -> impl Responder {
-    let result = usecase.perform().await;
+#[post("/something")]
+pub async fn something(
+    use_case: web::Data<Arc<SomethingUseCase>>,
+    body: web::Json<SomethingViewModel>,
+) -> impl Responder {
+    let result = use_case.perform(body.0).await;
     HttpResponse::Ok().json(result)
 }
