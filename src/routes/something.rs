0e5368018db3af84bd0d7b53ec16324a65111174
ralvescosta <rafael.rac.mg@gmail.com>
2021-08-25 07:50:47 -0300

    feat: router

--- src/routes/something.rs
+++ src/routes/something.rs
@@ -1,9 +1,5 @@
 use actix_web::web;
 
 pub fn register(server: &mut web::ServiceConfig) {
-    server.service(crate::controllers::something::index);
-    server.route(
-        "/something",
-        web::get().to(crate::controllers::something::something),
-    );
+    server.service(crate::controllers::something::something);
 }
